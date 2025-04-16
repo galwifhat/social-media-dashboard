0bcd34dfad4507af13d133a3f9044b931f232110
Johnson Alala <jonesalala95@gmail.com>
2025-04-17 01:19:33 +0300

    Added header, JSON data structure, and footer components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
-const Header = () => {
-  return <div>Header</div>;
-};
-
-export default Header;
+export default function Header() {
+  return (
+    <header>
+      <h1 className="title">SOCIAL MEDIA DASHBOARD</h1>
+      <p className="description">Group Two Mini Project</p>
+      <img
+        src="https://web.dev/static/articles/building/an-adaptive-favicon/image/example-dark-theme-favico-6a6f6f67bc3e7.png"
+        alt="dashboard logo"
+        style={{ width: "50px", height: "50px" }}
+      />
+    </header>
+  );
+}
